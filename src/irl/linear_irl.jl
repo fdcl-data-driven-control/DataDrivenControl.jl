f6c4ece0f531b1dd1914187a21e2ec2145039d2f
JinraeKim <kjl950403@gmail.com>
2021-12-30 23:23:46 +0900

    wip

--- src/irl/linear_irl.jl
+++ src/irl/linear_irl.jl
@@ -7,8 +7,6 @@ See [1, "Online Implementation of IRL: A Hybrid Optimal Adaptive Controller"].
 # Notes
 - T: Data stack period
 - N: The maximum length of stacked data
-- ϕs_prev: the vector of bases (evaluated)
-- V̂: the vector of approximate values (evaluated)
 """
 mutable struct LinearIRL <: AbstractIRL
     Q::AbstractMatrix
