9a48d295fe69c57217cdfeae41683a749ae27819
JinraeKim <kjl950403@gmail.com>
2022-01-03 23:07:38 +0900

    wip

--- src/DataDrivenControl.jl
+++ src/DataDrivenControl.jl
@@ -4,14 +4,6 @@ using LinearAlgebra
 using UnPack
 using Transducers
 
-## IRL
-# Linear IRL
-export LinearIRL, value_iteration!, policy_iteration!
-export optimal_input
-
-## Stop conditions
-export DistanceStopCondition
-
 ## Costs
 export QuadraticInInputCost, QuadraticCost
 
