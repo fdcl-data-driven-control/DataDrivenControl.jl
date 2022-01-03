bd637f1ba04ddb8e2ea3007522c2fc0972a4c03d
JinraeKim <kjl950403@gmail.com>
2022-01-03 19:18:22 +0900

    wip

--- src/adp/ct_vi_adp.jl
+++ src/adp/ct_vi_adp.jl
@@ -1,8 +1,9 @@
 """
+[1, Section IV.B]
 # Refs
 [1] T. Bian and Z.-P. Jiang, “Reinforcement Learning and Adaptive Optimal Control for Continuous-Time Nonlinear Systems: A Value Iteration Approach,” IEEE Trans. Neural Netw. Learning Syst., pp. 1–10, 2021, doi: 10.1109/TNNLS.2020.3045087.
 [2] T. Bian and Z.-P. Jiang, “Value Iteration, Adaptive Dynamic Programming, and Optimal Control of Nonlinear Systems,” in 2016 IEEE 55th Conference on Decision and Control (CDC), Las Vegas, NV, USA, Dec. 2016, pp. 3375–3380. doi: 10.1109/CDC.2016.7798777.
 """
-struct CTVIADP
+struct LinearCTVIADP
     # TODO
 end
